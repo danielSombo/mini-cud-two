c42152d3bc62f1527b8a75f1161e686586409c61
“Daniel <danielsombo55@gmail.com>
2025-02-19 20:10:15 +0000

    Modification backend

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const contactRoutes = require("./routes/ContactRoutes");
+const morgan = require("morgan");
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(morgan("combined"))
 
 // Routes
 app.use("/api/contacts", contactRoutes);
